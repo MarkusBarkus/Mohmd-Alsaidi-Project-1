5d3d6e05ccffe80cf86e14cfc75f6e1f9b7f47b7
Mohmd <m_alsaidi@fanshaweonline.ca>
2026-01-22 15:18:12 -0500

    Project 1 start

---
+++ App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, Button, TextInput } from 'react-native';
+
+import ListItem from './components/ListItem.js';
+
+import style from './style.js';
+
+function App() {
+
+    let [nextID, setNextID] = useState(1);
+    let [listItems, setListItems] = useState([]);
+
+    let clearList = () => setListItems([]);
+
+    function addItemToList() {
+        if (newItemText === '') return;
+        let newItem = { text: newItemText, id: nextID };
+        setNextID(nextID + 1);
+        setListItems([...listItems, newItem]);
+        setNewItemText('');
+    }
+
+    function removeItemFromList(idToRemove) {
+        let arrayWithRemovedItem = [];
+        listItems.forEach(item => {
+            if (item.id != idToRemove) {
+                arrayWithRemovedItem.push(item);
+            }
+        });
+        setListItems(arrayWithRemovedItem);
+    }
+
+    let [newItemText, setNewItemText] = useState('');
+    function onTextChanged(text) {
+        setNewItemText(text);
+    }
+
+    return (<View style={style.app}>
+        <StatusBar style="auto" />
+        <ListItem items={listItems} deleteItemCallback={removeItemFromList}></ListItem>
+        <TextInput style={style.inputText} value={newItemText} onChangeText={onTextChanged}></TextInput>
+        <View style={style.button} >
+            <Button title='ADD ITEM' onPress={addItemToList}></Button>
+        </View>
+        <View style={style.button} >
+            <Button title='CLEAR LIST' onPress={clearList}></Button>
+        </View>
+    </View>);
+}
+
+export default App;
